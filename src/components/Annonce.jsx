28b0e2f1574a7c344bcefa800bcb8b89cc1f5930
safidy54 <safidimalalarabezatovo@gmail.com>
2024-01-29 20:36:18 +0000

    Update Annonce.jsx

--- src/components/Annonce.jsx
+++ src/components/Annonce.jsx
@@ -3,91 +3,163 @@ import '../styles/Annonce.css'
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
 import { useEffect } from 'react';
-import {get} from '../axios_utils'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { get } from '../axios_utils'
 
 
-    function Annonce() {
+function Annonce() {
 
-        const [data, setData] = useState([]);
+    const navigate = useNavigate();
 
-        const representativeBodyTemplate = (rowData) => {
-            return rowData.marque;
-        };
+    const [data, setData] = useState([]);
 
-        const bodyModel = (rowData) => {
-            return rowData.model;
-        }
+    const representativeBodyTemplate = (rowData) => {
+        return rowData.marque;
+    };
 
-        const bodyCategorie = (rowData) => {
-            return rowData.categorie;
-        }
+    const bodyModel = (rowData) => {
+        return rowData.model;
+    }
 
-        const bodyPrix = (rowData) => {
-            return rowData.prix;
-        }
+    const bodyCategorie = (rowData) => {
+        return rowData.categorie;
+    }
 
-        const detail = (rowData) => {
-            
-            return (
-                <Link className='detail' to={'/Detail_Client/'+rowData.id}>
-                  {rowData.verified ='Voir detail' }
-                </Link>
-              );
-          };
+    const bodyPrix = (rowData) => {
+        return rowData.prix;
+    }
 
-        const convertDate = (rowData) => {
-            const timestamp = rowData.date;
-            const date = new Date(timestamp);
-            const formatted = date.toLocaleString(); 
-            return formatted;
-          };
-
-        const bodyDate_annonce = (rowData) => {
-            const timestamp = rowData.date_annonce;
-            const date = new Date(timestamp);
-            const formatted = date.toLocaleString(); 
-            return formatted;
-        }
-
-        useEffect(() => {
-            setTimeout(() => {
-               get('https://voiture-production-247e.up.railway.app/api/annonce_details/annonceNonValide')
-                    .then(response => {
-                        setData(response.data);
-                        console.log(response.data);
-                    })
-                    .catch(error => {
-                        console.error('Error fetching data:', error);
-                    });
-            }, 1000);
-        }, []);
-        
+    const detail = (rowData) => {
 
         return (
-            <div className='dashboard--content' >
+            <Link className='detail' to={'/Detail_Client/' + rowData.id}>
+                {rowData.verified = 'Voir detail'}
+            </Link>
+        );
+    };
 
-                <div className="input-card">
-                    <h4 className="annonce-title" style={{}}>Liste des annonces</h4>
-                    <DataTable className="custom-datatable" value={data}
-                        size="small"
-                        paginator rows={10}
-                        dataKey="id"
-                        tableStyle={{ minWidth: '15rem', width: '200px', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}
-                        emptyMessage="Donnees en attentes">
-                        <Column className='column' header="Marque" style={{ minWidth: '14rem' }} body={representativeBodyTemplate} />
-                        <Column className='column' header="Model" style={{ minWidth: '14rem' }} body={bodyModel} />
-                        <Column className='column' header="Categorie" style={{ minWidth: '14rem' }} body={bodyCategorie} />
-                        <Column className='column' header="Prix" style={{ minWidth: '14rem' }} body={bodyPrix} />
-                        <Column className='column' header="Date" style={{ minWidth: '14rem' }} body={bodyDate_annonce} />
-                    </DataTable>
-                </div>
+    const convertDate = (rowData) => {
+        const timestamp = rowData.date;
+        const date = new Date(timestamp);
+        const formatted = date.toLocaleString();
+        return formatted;
+    };
+
+    const bodyDate_annonce = (rowData) => {
+        const timestamp = rowData.date_annonce;
+        const date = new Date(timestamp);
+        const formatted = date.toLocaleString();
+        return formatted;
+    }
+
+    const actionTemplate = (rowData) => {
+        return (
+            <div>
+                {/* Validate Button */}
+                <Button
+                    label='Valider'
+                    icon='pi pi-check'
+                    onClick={() => handleValidation(rowData.id_annonce)}
+                />
+                {/* Refuser Button */}
+                <Button
+                    label='Refuser'
+                    icon='pi pi-times'
+                    onClick={() => handleRefusal(rowData.id_annonce)}
+                    className='p-button-danger'
+                />
+            </div>
+        );
+    };
+
+    const handleValidation = async (id) => {
+        const url = `https://voiture-production-247e.up.railway.app/api/annonce/validation/${id}`;
+        const token = localStorage.getItem('token');
+    
+        try {
+            const config = {
+                method: 'post',
+                url: url,
+                headers: {
+                    'authorization': token
+                },
+            };
+    
+            const response = await axios.request(config);
+            console.log(response.data);
+            navigate("/Home")
+        } catch (error) {
+            console.error('Error validating announcement:', error);
+        }
+    };
+
+    const handleRefusal = async (id) => {
+        const url = `https://voiture-production-247e.up.railway.app/api/annonce/refus/${id}`;
+        const token = localStorage.getItem('token');
+        
+        try {
+            let config = {
+                method: 'post',
+                url: url,
+                headers: {
+                    'authorization': token
+                },
+            };
+            
+            const response = await axios.request(config);
+            console.log(response.data);
+            navigate("/Home")
+            // Handle the response as needed
+        } catch (error) {
+            console.error('Error refusing announcement:', error);
+            // Handle errors, e.g., show an error message to the user
+        }
+    };
+    
+
+    useEffect(() => {
+        setTimeout(() => {
+            get('https://voiture-production-247e.up.railway.app/api/annonce_details/annonceNonValide')
+                .then(response => {
+                    setData(response.data);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }, 1000);
+    }, []);
 
 
+
+    return (
+        <div className='dashboard--content' >
+
+            <div className="input-card">
+                <h4 className="annonce-title" style={{}}>Liste des annonces</h4>
+                <DataTable className="custom-datatable" value={data}
+                    size="small"
+                    paginator rows={10}
+                    dataKey="id"
+                    tableStyle={{ minWidth: '15rem', width: '200px', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}
+                    emptyMessage="Donnees en attentes">
+                    <Column className='column' header="Marque" style={{ minWidth: '14rem' }} body={representativeBodyTemplate} />
+                    <Column className='column' header="Model" style={{ minWidth: '14rem' }} body={bodyModel} />
+                    <Column className='column' header="Categorie" style={{ minWidth: '14rem' }} body={bodyCategorie} />
+                    <Column className='column' header="Prix" style={{ minWidth: '14rem' }} body={bodyPrix} />
+                    <Column className='column' header="Date" style={{ minWidth: '14rem' }} body={bodyDate_annonce} />
+                    <Column header="Actions" body={actionTemplate} />
+                </DataTable>
             </div>
 
 
-        )
-    }
+        </div>
+
+
+    )
+}
 
 export default Annonce
